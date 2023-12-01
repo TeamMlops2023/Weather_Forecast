60166d259bf1b1b467e1bca6a10dfec941763ee4
TeamMlops2023 <150617746+TeamMlops2023@users.noreply.github.com>
2023-12-01 14:16:00 +0100

    Create test_endpoint.py
    crdation de 1er test qui va checker le endpoint metrics

---
+++ tests/test_endpoint.py
@@ -0,0 +1,10 @@
+from fastapi.testclient import TestClient
+from app import app
+
+client = TestClient(app)
+
+# test de l'endpoint metrics
+def test_metrics_endpoint():
+    response = client.get("/metrics")
+    assert response.status_code == 200
+    assert "model_execution_count" in response.text
