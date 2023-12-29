6d069e9a43dfe9bd4b3f41762f5075762acb3531
Abicakci <121668685+Abicakci@users.noreply.github.com>
2023-12-29 14:48:44 +0100

    Update modele.py

--- machine_learning/modele.py
+++ machine_learning/modele.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, jsonify
 import joblib
+import numpy as np
 
 app = Flask(__name__)
 
@@ -9,11 +10,16 @@ model = joblib.load('model.joblib')
 @app.route('/predict', methods=['POST'])
 def predict():
     data = request.get_json(force=True)
-    # Transformez 'data' en format approprié pour le modèle
-    # Par exemple :
-    model_input = [data['feature1'], data['feature2'], ...]
-    prediction = model.predict([model_input])
-    return jsonify({"result": prediction})
+    # Adaptez ces lignes pour correspondre aux caractéristiques attendues par votre modèle
+    date = data.get('date')
+    city = data.get('city')
+
+    # Vous devez transformer 'date' et 'city' en un format numérique que votre modèle peut comprendre.
+    # Ceci est juste un exemple. Vous devrez adapter cela en fonction de la façon dont votre modèle a été formé.
+    model_input = [np.array([date, city])]
+
+    prediction = model.predict(model_input)
+    return jsonify({"result": prediction.tolist()})  # Convertit le résultat en liste pour la sérialisation JSON
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5000)
