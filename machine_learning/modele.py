ea61f7eb678be646de7a67045b5513b212fee54b
Abicakci <121668685+Abicakci@users.noreply.github.com>
2023-12-29 20:16:31 +0100

    Update modele.py

--- machine_learning/modele.py
+++ machine_learning/modele.py
@@ -1,41 +1,34 @@
-from flask import Flask, request, jsonify
 import pandas as pd
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder
+import joblib
+import os
 
-app = Flask(__name__)
+# Chemin vers le fichier de données
+chemin_fichier_donnees = os.path.join('data', 'data_features_with_location.csv')
 
-# Fonction pour charger les données et entraîner le modèle
-def train_model():
-    df = pd.read_csv('data/data_features_with_location.csv')
-    df['date'] = pd.to_datetime(df[['year', 'month', 'day']])
-    
-    le = LabelEncoder()
-    df['location_encoded'] = le.fit_transform(df['location'])
-    
-    X = df.drop(['raintomorrow', 'year', 'month', 'day', 'location', 'date'], axis=1)
-    y = df['raintomorrow']
-    
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
-    model = RandomForestClassifier()
-    model.fit(X_train, y_train)
-    
-    return model, le
+# Charger les données
+df = pd.read_csv(chemin_fichier_donnees)
+df['date'] = pd.to_datetime(df[['year', 'month', 'day']])
 
-# Entraîner le modèle au démarrage du service
-model, le = train_model()
+# Encodage des variables catégorielles
+le = LabelEncoder()
+df['location_encoded'] = le.fit_transform(df['location'])
 
-@app.route('/predict', methods=['POST'])
-def predict():
-    data = request.get_json(force=True)
-    # Convertissez 'date' et 'city' en entrée pour votre modèle ici
-    # Par exemple, encodez la ville avec le LabelEncoder et traitez la date comme nécessaire
-    city_encoded = le.transform([data['city']])
-    # Préparez l'entrée du modèle en fonction de votre modèle spécifique
+# Préparation des données pour l'entraînement
+X = df.drop(['raintomorrow', 'year', 'month', 'day', 'location', 'date'], axis=1)
+y = df['raintomorrow']
 
-    prediction = model.predict([city_encoded])
-    return jsonify({"result": prediction.tolist()})
+# Séparation des données en ensembles d'entraînement et de test
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+# Entraînement du modèle
+model = RandomForestClassifier()
+model.fit(X_train, y_train)
+
+# Prédiction
+predictions = model.predict(X_test)
+
+# Affichage des prédictions
+print(predictions)
