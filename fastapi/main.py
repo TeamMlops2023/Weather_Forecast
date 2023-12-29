f864bccedb28487a7d3b95167786b8243b7e5fb5
Abicakci <121668685+Abicakci@users.noreply.github.com>
2023-12-29 10:27:58 +0100

    Update main.py

--- fastapi/main.py
+++ fastapi/main.py
@@ -1,23 +1,16 @@
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI
 import requests
 
 app = FastAPI()
 
 @app.get("/run_model")
 def run_model(date: str, city: str):
-    # Structurer les données pour le modèle ML
-    data_to_send = {
-        "Date": date,
-        "City": city
+    # Préparer les données pour le modèle ML
+    data = {
+        "date": date,
+        "city": city
     }
 
-    # Remplacer l'URL ci-dessous par l'URL réelle de votre service ML
-    ml_service_url = "http://ml-service:5000/predict"
-    
-    try:
-        # Envoyer les données au service ML et obtenir la réponse
-        response = requests.post(ml_service_url, json=data_to_send)
-        response.raise_for_status()  # Cela va déclencher une exception si la requête échoue
-        return response.json()
-    except requests.RequestException as e:
-        raise HTTPException(status_code=503, detail=str(e))
+    # Envoyer les données au service ML et obtenir la réponse
+    response = requests.post("http://ml-service:5000/predict", json=data)
+    return response.json()
