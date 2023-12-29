a2ca89b2b18af2ddf697a67bd2252713f4890002
Abicakci <121668685+Abicakci@users.noreply.github.com>
2023-12-29 15:05:38 +0100

    Update modele.py

--- machine_learning/modele.py
+++ machine_learning/modele.py
@@ -1,34 +1,41 @@
 from flask import Flask, request, jsonify
-import joblib
 import pandas as pd
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
 from sklearn.preprocessing import LabelEncoder
+import os
 
 app = Flask(__name__)
 
-# Charger le modèle pré-entraîné
-model, le = joblib.load('model_with_encoder.joblib')
+# Fonction pour charger les données et entraîner le modèle
+def load_and_train_model():
+    df = pd.read_csv('data/data_features_with_location.csv', index_col=0)
+    df['date'] = pd.to_datetime(df[['year', 'month', 'day']])
+    le = LabelEncoder()
+    df['location_encoded'] = le.fit_transform(df['location'])
+    X = df.drop(['raintomorrow', 'year', 'month', 'day', 'location', 'date'], axis=1)
+    y = df['raintomorrow']
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+    model = RandomForestClassifier()
+    model.fit(X_train, y_train)
+    return model, le
+
+model, le = load_and_train_model()
 
 @app.route('/predict', methods=['POST'])
 def predict():
     data = request.get_json(force=True)
-    date = data['date']
-    city = data['city']
+    # Adaptez cette partie pour accepter la date et la ville en entrée
+    # Exemple : date = data['date'], city = data['city']
+    # Transformez ces entrées en un format approprié pour votre modèle
+    # Par exemple, encodez la ville avec le LabelEncoder
 
-    # Convertir la date en format datetime
-    date = pd.to_datetime(date, format='%Y-%m-%d')
+    # Ici, vous devez ajouter la logique pour transformer 'date' et 'city' en entrée pour votre modèle
 
-    # Préparer les données pour le modèle
-    # (Cela doit correspondre à la manière dont vous avez formé votre modèle)
-    model_input = pd.DataFrame({
-        'year': [date.year],
-        'month': [date.month],
-        'day': [date.day],
-        'location_encoded': le.transform([city])
-    })
-
-    # Faire la prédiction
-    prediction = model.predict(model_input)
-    return jsonify({"prediction": int(prediction[0])})
+    # Effectuez la prédiction
+    prediction = model.predict([model_input])
+    return jsonify({"result": prediction})
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5000)
