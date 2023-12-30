34014322d86770dbbfe18eab100376e759b828ee
Abicakci <121668685+Abicakci@users.noreply.github.com>
2023-12-30 21:55:06 +0100

    Update main.py

--- fastapi/main.py
+++ fastapi/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Query
 import uvicorn
 
 app = FastAPI()
@@ -6,6 +6,14 @@ app = FastAPI()
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
+
+@app.get("/status")
+def get_status():
+    return {"status": "ok"}
+
+@app.get("/echo")
+def echo(text: str = Query(None, min_length=1, max_length=100)):
+    return {"echo": text}
 
 if __name__ == "__main__":
     uvicorn.run("app:app", host="0.0.0.0", port=8000, log_level="debug")
