46f8c13d7be2a7c067e17bc374bc9d865a88b4b4
Abicakci <121668685+Abicakci@users.noreply.github.com>
2023-12-31 18:24:27 +0100

    Update test_fastapi.py

--- fastapi/tests/test_fastapi.py
+++ fastapi/tests/test_fastapi.py
@@ -1,35 +1,23 @@
+# test_fastapi.py
+import unittest
 import requests
-import argparse
 
-# Configuration du parser d'arguments pour recevoir l'adresse IP du conteneur
-parser = argparse.ArgumentParser(description='Test FastAPI Endpoints.')
-parser.add_argument('--container-ip', required=True, help='IP address of the FastAPI container')
-args = parser.parse_args()
+class TestFastAPIEndpoints(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.base_url = "http://172.17.0.3:8000"  # Remplacez par l'IP du conteneur
 
-# Définir l'URL de base de l'API
-BASE_URL = f"http://{args.container_ip}:8000"
+    def test_root_endpoint(self):
+        response = requests.get(f"{self.base_url}/")
+        self.assertEqual(response.status_code, 200)
+        self.assertIn("Hello World", response.text)
 
-def test_root_endpoint():
-    """ Teste l'endpoint racine. """
-    response = requests.get(f"{BASE_URL}/")
-    assert response.status_code == 200
-    assert "Hello World" in response.text
+    def test_status_endpoint(self):
+        response = requests.get(f"{self.base_url}/status")
+        self.assertEqual(response.status_code, 200)
+        self.assertIn("ok", response.text)
 
-def test_status_endpoint():
-    """ Teste l'endpoint /status. """
-    response = requests.get(f"{BASE_URL}/status")
-    assert response.status_code == 200
-    assert "ok" in response.text
+    # ... autres tests ...
 
-def test_echo_endpoint():
-    """ Teste l'endpoint /echo avec un paramètre. """
-    test_message = "HelloJenkins"
-    response = requests.get(f"{BASE_URL}/echo", params={"text": test_message})
-    assert response.status_code == 200
-    assert test_message in response.text
-
-# Ajoutez ici d'autres tests pour d'autres endpoints si nécessaire
-
-if __name__ == "__main__":
-    import pytest
-    pytest.main()
+if __name__ == '__main__':
+    unittest.main()
