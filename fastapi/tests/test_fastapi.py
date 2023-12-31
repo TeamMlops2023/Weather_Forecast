4a504195a24b59df2c48c948a3534ae8cc16ee63
Abicakci <121668685+Abicakci@users.noreply.github.com>
2023-12-31 18:43:16 +0100

    Update test_fastapi.py

--- fastapi/tests/test_fastapi.py
+++ fastapi/tests/test_fastapi.py
@@ -4,21 +4,20 @@ import requests
 class TestFastAPIEndpoints(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
-        # Remplacez par l'adresse IP de votre conteneur FastAPI
-        cls.base_url = "http://172.17.0.3:8000"
+        cls.base_url = "http://172.17.0.3:8000"  # Remplacez par l'adresse IP du conteneur
 
     def test_root_endpoint(self):
         """Test du endpoint racine."""
         response = requests.get(f"{self.base_url}/")
         self.assertEqual(response.status_code, 200)
-        self.assertIn("Hello World", response.text)
+        self.assertEqual(response.json(), {"Hello": "World"})
 
     def test_status_endpoint(self):
         """Test du endpoint /status."""
         response = requests.get(f"{self.base_url}/status")
         self.assertEqual(response.status_code, 200)
         # Adaptez cette vérification au contenu attendu de la réponse
-        self.assertIn("status", response.text)
+        self.assertEqual(response.json(), {"status": "ok"})
 
     def test_echo_endpoint(self):
         """Test du endpoint /echo."""
