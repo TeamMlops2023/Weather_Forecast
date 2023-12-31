99bf791e54f9c9539add6f3761b79ef3ed39085a
Abicakci <121668685+Abicakci@users.noreply.github.com>
2023-12-31 18:40:48 +0100

    Update test_fastapi.py

--- fastapi/tests/test_fastapi.py
+++ fastapi/tests/test_fastapi.py
@@ -1,23 +1,33 @@
-# test_fastapi.py
 import unittest
 import requests
 
 class TestFastAPIEndpoints(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
-        cls.base_url = "http://172.17.0.3:8000"  # Remplacez par l'IP du conteneur
+        # Remplacez par l'adresse IP de votre conteneur FastAPI
+        cls.base_url = "http://172.17.0.3:8000"
 
     def test_root_endpoint(self):
+        """Test du endpoint racine."""
         response = requests.get(f"{self.base_url}/")
         self.assertEqual(response.status_code, 200)
-        self.assertIn('"Hello" : "World"', response.text)
+        self.assertIn("Hello World", response.text)
 
     def test_status_endpoint(self):
+        """Test du endpoint /status."""
         response = requests.get(f"{self.base_url}/status")
         self.assertEqual(response.status_code, 200)
-        self.assertIn("ok", response.text)
+        # Adaptez cette vérification au contenu attendu de la réponse
+        self.assertIn("status", response.text)
 
-    # ... autres tests ...
+    def test_echo_endpoint(self):
+        """Test du endpoint /echo."""
+        test_message = "HelloJenkins"
+        response = requests.get(f"{self.base_url}/echo", params={"text": test_message})
+        self.assertEqual(response.status_code, 200)
+        self.assertIn(test_message, response.text)
+
+    # Ajoutez d'autres tests pour les endpoints supplémentaires ici
 
 if __name__ == '__main__':
     unittest.main()
