d337a451c151c1d0a56da34de7c940cff32f322d
Abicakci <121668685+Abicakci@users.noreply.github.com>
2024-01-05 16:52:08 +0100

    Create init.sql

---
+++ database/init.sql
@@ -0,0 +1,13 @@
+-- Vérifiez si la base de données existe, sinon créez-la
+CREATE DATABASE IF NOT EXISTS mlops_weather;
+USE mlops_weather;
+
+-- Créez la table weather_predictions si elle n'existe pas
+CREATE TABLE IF NOT EXISTS weather_predictions (
+    id INT PRIMARY KEY,
+    date DATE,
+    location VARCHAR(255),
+    prediction INT,
+    accuracy FLOAT
+);
+
