044131ffd930ee8b68fb646d4644922cbf85bbdd
Abicakci <121668685+Abicakci@users.noreply.github.com>
2024-01-07 21:53:43 +0100

    Update modele.py

--- machine_learning/modele.py
+++ machine_learning/modele.py
@@ -8,8 +8,8 @@ import mysql.connector
 from mysql.connector import Error
 import time
 
-# Pause pendant 2 minutes (120 secondes)
-time.sleep(120)
+# Pause pendant 1 minutes (60 secondes)
+time.sleep(60)
 
 # Paramètres de réessai de connexion
 max_attempts = 5
