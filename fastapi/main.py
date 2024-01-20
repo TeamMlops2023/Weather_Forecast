51aea07cffc48dd41b9a54126e157d3665bc5d0a
Abicakci <121668685+Abicakci@users.noreply.github.com>
2024-01-20 22:26:33 +0100

    Update main.py

--- fastapi/main.py
+++ fastapi/main.py
@@ -1,35 +1,31 @@
+from fastapi import FastAPI
 from fastapi import FastAPI, Query
 import uvicorn
-from prometheus_fastapi_instrumentator import Instrumentator, metrics
 
 # Création d'une instance FastAPI
 app = FastAPI()
-
-# Configurez l'instrumentation Prometheus
-Instrumentator().instrument(app).expose(app)
 
 # Définition de l'endpoint racine ("/")
 @app.get("/")
 def read_root():
     # Renvoie un message JSON lorsque quelqu'un accède à la racine de l'application
     return {"Hello": "World"}
-
 # Définition de l'endpoint "/status"
 @app.get("/status")
 def get_status():
     # Renvoie un statut JSON "ok" lorsque quelqu'un accède à la route "/status"
     return {"status": "ok"}
-
 # Définition de l'endpoint "/echo" avec un paramètre de requête textuel
 @app.get("/echo")
 def echo(text: str = Query(None, min_length=1, max_length=100)):
     # Valide le paramètre de requête et le renvoie dans une réponse JSON
     return {"echo": text}
 
-# Ajoutez une route pour exporter les métriques Prometheus au chemin "/metrics"
-@app.get("/metrics")
-async def get_metrics():
-    return metrics()
+    # Faire la prédiction
+    prediction = model.predict(input_data)
+
+    # Renvoyer la prédiction
+    return {"prediction": prediction.tolist()}
 
 # Vérifie si le script est exécuté en tant que fichier principal
 if __name__ == "__main__":
