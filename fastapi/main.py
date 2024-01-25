e3eaa9db76a741ff2a5e9d9d7c387b28fb7b30bf
Abicakci <121668685+Abicakci@users.noreply.github.com>
2024-01-25 22:32:17 +0100

    Update main.py

--- fastapi/main.py
+++ fastapi/main.py
@@ -12,30 +12,25 @@ app = FastAPI()
 # Instrumentation pour Prometheus
 Instrumentator().instrument(app).expose(app)
 
-# Création de la connection à la base de données
+# Création de la connexion à la base de données
 # Variables de connexion
-mysql_url = os.environ.get('MYSQL_URL')
-mysql_user = 'root'
-mysql_password = 'mysecretpassword'  # À compléter
+mysql_url = os.environ.get('MYSQL_URL', 'database-service')  # Utilisez le nom du service Kubernetes comme hôte par défaut
+mysql_user = os.environ.get('MYSQL_USER', 'mlops')           # Utilisateur par défaut 'mlops'
+mysql_password = os.environ.get('MYSQL_PASSWORD', 'mlops')   # Mot de passe par défaut 'mlops'
 database_name = 'mlops_weather'
 
 # Création de l'URL de connexion
-connection_url = 'mysql+pymysql://{user}:{password}@{url}/{database}'.format(
-    user=mysql_user,
-    password=mysql_password,
-    url=mysql_url,
-    database=database_name
-)
+connection_url = f'mysql+pymysql://{mysql_user}:{mysql_password}@{mysql_url}/{database_name}'
 
 # Création de la connexion
 mysql_engine = create_engine(connection_url)
 
 # Création de la classe de modèle pour la prédiction
 class Prediction(BaseModel):
-    date: datetime = None
-    location: str = None
-    prediction: int = None
-    accuracy: float = None
+    date: datetime
+    location: str
+    prediction: int
+    accuracy: float
 
 # Définition de l'endpoint racine ("/")
 @app.get("/")
@@ -53,30 +48,32 @@ async def echo(text: str = Query(None, min_length=1, max_length=100)):
     return {"echo": text}
 
 # Définition de l'endpoint "/prediction" avec un paramètre de requête textuel
-@app.get('/prediction/{location:str}', response_model=Prediction)
-async def get_prediction(location):
+@app.get('/prediction/{location}', response_model=Prediction)
+async def get_prediction(location: str):
     date_format = "%Y-%m-%d"
     tomorrow = datetime.now() + timedelta(days=1)
     tomorrow = tomorrow.strftime(date_format)
 
     with mysql_engine.connect() as connection:
         results = connection.execute(
-            "SELECT date, location, prediction, accuracy FROM weather_predictions WHERE location = '{}' AND date = '{}';".format(location, tomorrow))
-
+            "SELECT date, location, prediction, accuracy FROM weather_predictions WHERE location = %s AND date = %s;",
+            (location, tomorrow)
+        )
         result_list = [
             Prediction(
                 date=i[0],
                 location=i[1],
                 prediction=i[2],
                 accuracy=i[3]
-            ) for i in results.fetchall()]
+            ) for i in results.fetchall()
+        ]
 
-    if len(result_list) == 0:
+    if not result_list:
         raise HTTPException(
             status_code=404,
-            detail='Prédiction non trouvée')
-    else:
-        return result_list[0]
+            detail='Prediction not found'
+        )
+    return result_list[0]
 
 # Vérifie si le script est exécuté en tant que fichier principal
 if __name__ == "__main__":
