85d04ed45657bf8211bb9b4bb1d81a211dc0c6bf
Abicakci <121668685+Abicakci@users.noreply.github.com>
2024-01-26 15:00:26 +0100

    Update main.py

--- fastapi/main.py
+++ fastapi/main.py
@@ -1,51 +1,57 @@
 from fastapi import FastAPI, HTTPException
-from pydantic import BaseModel
 from sqlalchemy import create_engine, text
+from sqlalchemy.orm import sessionmaker
+from fastapi import FastAPI, Query
+from pydantic import BaseModel
 from datetime import date
 import os
-
-# Création d'une instance FastAPI
 app = FastAPI()
-
 # Variables de connexion à la base de données
 mysql_url = os.environ.get('MYSQL_URL', 'database-service')
 mysql_user = os.environ.get('MYSQL_USER', 'mlops')
 mysql_password = os.environ.get('MYSQL_PASSWORD', 'mlops')
 database_name = 'mlops_weather'
-
 # Création de l'URL de connexion
 connection_url = f'mysql+pymysql://{mysql_user}:{mysql_password}@{mysql_url}/{database_name}'
-
 # Création de la connexion
 mysql_engine = create_engine(connection_url)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=mysql_engine)
 
-# Modèle pour les données historiques
-class HistoricalData(BaseModel):
+
+class WeatherPrediction(BaseModel):
+    id: int
     date: date
     location: str
     prediction: int
     accuracy: float
+# Endpoint racine
+@app.get("/")
+async def read_root():
+    return {"Hello": "World"}
+# Endpoint pour vérifier le statut
+@app.get("/status")
+async def get_status():
+    return {"status": "ok"}
+# Endpoint pour l'écho
+@app.get("/echo")
+async def echo(text: str = Query(None, min_length=1, max_length=100)):
+    return {"echo": text}
+@app.get("/predictions/")
+def get_weather_predictions():
+    # Créez une session SQLAlchemy
+    db = SessionLocal()
 
-# Endpoint pour obtenir les données historiques d'une ville
-@app.get("/predictions")
-async def get_predictions(city: str):
-    query = text("""
-        SELECT date, location, prediction, accuracy 
-        FROM weather_predictions 
-        WHERE location = :city
-        ORDER BY date DESC
-        LIMIT 1;
-    """)
-    
-    with mysql_engine.connect() as connection:
-        result = connection.execute(query, {'city': city}).fetchone()
+    try:
+        # Exécutez une requête SQL pour récupérer les prédictions météo
+        query = text("SELECT * FROM weather_predictions")
+        query = text("SELECT date, location, prediction, accuracy FROM weather_predictions WHERE location = 'Sydney' AND date BETWEEN '2014-01-16' AND '2014-01-17'")
+        results = db.execute(query).fetchall()
 
-    if not result:
-        raise HTTPException(status_code=404, detail="No predictions found for the city")
-
-    return HistoricalData(date=result[0], location=result[1], prediction=result[2], accuracy=result[3])
-
-# Exécuter l'application si c'est le fichier principal
-if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run("app:app", host="0.0.0.0", port=8000, log_level="debug")
+        # Transformez les résultats en liste de dictionnaires
+        predictions = [dict(result) for result in results]
+        return predictions
+    except Exception as e:
+        raise HTTPException(status_code=500, detail="Internal Server Error")
+    finally:
+        # Fermez la session
+        db.close()
