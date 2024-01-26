c3bec9114c9e8e0598eb7a61ac4102efe353ef15
Abicakci <121668685+Abicakci@users.noreply.github.com>
2024-01-26 15:05:37 +0100

    Update main.py

--- fastapi/main.py
+++ fastapi/main.py
@@ -1,22 +1,26 @@
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, HTTPException, Query
 from sqlalchemy import create_engine, text
 from sqlalchemy.orm import sessionmaker
-from fastapi import FastAPI, Query
 from pydantic import BaseModel
 from datetime import date
 import os
+
 app = FastAPI()
+
+# Adresse IP du conteneur MySQL (remplacez par l'adresse IP appropriée)
+mysql_ip = '10.96.39.152'  # Remplacez ceci par l'adresse IP de votre conteneur MySQL
+
 # Variables de connexion à la base de données
-mysql_url = os.environ.get('MYSQL_URL', 'database-service')
 mysql_user = os.environ.get('MYSQL_USER', 'mlops')
 mysql_password = os.environ.get('MYSQL_PASSWORD', 'mlops')
 database_name = 'mlops_weather'
-# Création de l'URL de connexion
-connection_url = f'mysql+pymysql://{mysql_user}:{mysql_password}@{mysql_url}/{database_name}'
+
+# Création de l'URL de connexion en utilisant l'adresse IP
+connection_url = f'mysql+pymysql://{mysql_user}:{mysql_password}@{mysql_ip}/{database_name}'
+
 # Création de la connexion
 mysql_engine = create_engine(connection_url)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=mysql_engine)
-
 
 class WeatherPrediction(BaseModel):
     id: int
@@ -24,34 +28,35 @@ class WeatherPrediction(BaseModel):
     location: str
     prediction: int
     accuracy: float
-# Endpoint racine
+
 @app.get("/")
 async def read_root():
     return {"Hello": "World"}
-# Endpoint pour vérifier le statut
+
 @app.get("/status")
 async def get_status():
     return {"status": "ok"}
-# Endpoint pour l'écho
+
 @app.get("/echo")
 async def echo(text: str = Query(None, min_length=1, max_length=100)):
     return {"echo": text}
+
 @app.get("/predictions/")
-def get_weather_predictions():
-    # Créez une session SQLAlchemy
+def get_weather_predictions(location: str, date: date):
     db = SessionLocal()
 
     try:
-        # Exécutez une requête SQL pour récupérer les prédictions météo
-        query = text("SELECT * FROM weather_predictions")
-        query = text("SELECT date, location, prediction, accuracy FROM weather_predictions WHERE location = 'Sydney' AND date BETWEEN '2014-01-16' AND '2014-01-17'")
-        results = db.execute(query).fetchall()
+        query = text("SELECT date, location, prediction, accuracy FROM weather_predictions "
+                     "WHERE location = :location AND date = :date")
+        results = db.execute(query, {'location': location, 'date': date}).fetchall()
 
-        # Transformez les résultats en liste de dictionnaires
         predictions = [dict(result) for result in results]
         return predictions
     except Exception as e:
         raise HTTPException(status_code=500, detail="Internal Server Error")
     finally:
-        # Fermez la session
         db.close()
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, log_level="info")
