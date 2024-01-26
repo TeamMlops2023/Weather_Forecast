b877c35eb9966bad1d4bc91f08d345629c896692
Abicakci <121668685+Abicakci@users.noreply.github.com>
2024-01-26 10:32:49 +0100

    Update main.py

--- fastapi/main.py
+++ fastapi/main.py
@@ -1,26 +1,26 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from sqlalchemy import create_engine, text
 from sqlalchemy.orm import sessionmaker
 from pydantic import BaseModel
-from datetime import date 
+from datetime import date
 import os
 
-# Récupérez les variables d'environnement pour la connexion à la base de données
+app = FastAPI()
+
+# Variables de connexion à la base de données
 mysql_url = os.environ.get('MYSQL_URL', 'database-service')
 mysql_user = os.environ.get('MYSQL_USER', 'mlops')
 mysql_password = os.environ.get('MYSQL_PASSWORD', 'mlops')
 database_name = 'mlops_weather'
 
-# Créez l'URL de connexion à la base de données
+# Création de l'URL de connexion
 connection_url = f'mysql+pymysql://{mysql_user}:{mysql_password}@{mysql_url}/{database_name}'
 
-# Configuration de la connexion à la base de données
-engine = create_engine(connection_url)
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+# Création de la connexion
+mysql_engine = create_engine(connection_url)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=mysql_engine)
 
-app = FastAPI()
 
-# Modèle de données pour les résultats de la requête
 class WeatherPrediction(BaseModel):
     id: int
     date: date
@@ -28,20 +28,23 @@ class WeatherPrediction(BaseModel):
     prediction: int
     accuracy: float
 
-# Endpoint pour interroger la base de données
+
 @app.get("/predictions/")
 def get_weather_predictions():
     # Créez une session SQLAlchemy
     db = SessionLocal()
 
-    # Exécutez une requête SQL pour récupérer les prédictions météo
-    query = text("SELECT * FROM weather_predictions")
-    results = db.execute(query).fetchall()
+    try:
+        # Exécutez une requête SQL pour récupérer les prédictions météo
+        query = text("SELECT * FROM weather_predictions")
+        results = db.execute(query).fetchall()
 
-    # Transformez les résultats en liste de dictionnaires
-    predictions = [dict(result) for result in results]
+        # Transformez les résultats en liste de dictionnaires
+        predictions = [dict(result) for result in results]
 
-    # Fermez la session
-    db.close()
-
-    return predictions
+        return predictions
+    except Exception as e:
+        raise HTTPException(status_code=500, detail="Internal Server Error")
+    finally:
+        # Fermez la session
+        db.close()
