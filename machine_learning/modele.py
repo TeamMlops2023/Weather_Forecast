c247ec2b91713dc8a2a1a8689f1365f36995f8b2
Abicakci <121668685+Abicakci@users.noreply.github.com>
2024-01-27 23:04:55 +0100

    Update modele.py

--- machine_learning/modele.py
+++ machine_learning/modele.py
@@ -3,11 +3,12 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder
 import mysql.connector
-import os
 from mysql.connector import Error
+import os
 import time
 
 # Pause pendant 20 secondes pour s'assurer que la base de données est prête
+print("Attente de 20 secondes avant de commencer...")
 time.sleep(20)
 
 # Paramètres de réessai de connexion
@@ -16,7 +17,8 @@ attempt_count = 0
 
 while attempt_count < max_attempts:
     try:
-        # Connexion à la base de données
+        # Tentative de connexion à la base de données
+        print(f"Tentative de connexion à la base de données, essai {attempt_count + 1}")
         db = mysql.connector.connect(
             host="database-service",
             user="root",
@@ -29,7 +31,7 @@ while attempt_count < max_attempts:
     except Error as e:
         print(f"Erreur lors de la connexion à MySQL: {e}")
         attempt_count += 1
-        time.sleep(5)  # Attendre 5 secondes avant de réessayer
+        time.sleep(5)
 
 if attempt_count == max_attempts:
     print("Échec de la connexion à MySQL après plusieurs tentatives")
@@ -37,14 +39,17 @@ if attempt_count == max_attempts:
 
 # Chemin vers le fichier de données
 chemin_fichier_donnees = 'data/data_features_with_location.csv'
+print(f"Chargement des données depuis {chemin_fichier_donnees}")
 
 # Charger les données
 df = pd.read_csv(chemin_fichier_donnees)
 df['date'] = pd.to_datetime(df[['year', 'month', 'day']])
+print("Données chargées avec succès.")
 
 # Encodage des variables catégorielles
 le = LabelEncoder()
 df['location_encoded'] = le.fit_transform(df['location'])
+print("Variables catégorielles encodées.")
 
 # Préparation des données pour l'entraînement
 X = df.drop(['raintomorrow', 'year', 'month', 'day', 'location', 'date'], axis=1)
@@ -52,20 +57,24 @@ y = df['raintomorrow']
 
 # Séparation des données en ensembles d'entraînement et de test
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+print("Données séparées en ensembles d'entraînement et de test.")
 
 # Entraînement du modèle
 model = RandomForestClassifier()
+print("Début de l'entraînement du modèle...")
 model.fit(X_train, y_train)
+print("Modèle entraîné.")
 
 # Prédiction
 predictions = model.predict(X_test)
+print("Prédictions réalisées.")
 
-# Insertion des prédictions dans la base de données au fur et à mesure qu'elles sont générées
+# Insertion des prédictions dans la base de données
 for i in range(len(predictions)):
     date = df.iloc[i]['date']
     location = df.iloc[i]['location']
-    prediction = predictions[i].item()  # Convertit numpy.int64 en int
-    accuracy = model.score(X_test, y_test)  # Calcul de l'exactitude du modèle
+    prediction = predictions[i].item()
+    accuracy = model.score(X_test, y_test)
 
     # Vérification si l'entrée existe déjà
     check_query = "SELECT EXISTS(SELECT 1 FROM weather_predictions WHERE date=%s AND location=%s)"
@@ -75,15 +84,17 @@ for i in range(len(predictions)):
     if not exists:
         insert_query = "INSERT INTO weather_predictions (date, location, prediction, accuracy) VALUES (%s, %s, %s, %s)"
         cursor.execute(insert_query, (date, location, prediction, accuracy))
+        print(f"Insertion de la prédiction pour {date} et {location}.")
     else:
         print(f"Entrée pour {date} et {location} existe déjà.")
 
 # Commit des modifications dans la base de données
 db.commit()
+print("Modifications commitées dans la base de données.")
 
 # Fermeture de la connexion à la base de données
 cursor.close()
 db.close()
 
 # Affichage des prédictions
-print("Prédictions effectuées avec succès.")
+print("Prédictions effectuées avec succès et enregistrées dans la base de données.")
