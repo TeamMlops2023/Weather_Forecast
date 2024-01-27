3ed5c00a179c52693dd634cb0588cbc47c436302
Abicakci <121668685+Abicakci@users.noreply.github.com>
2024-01-27 20:36:57 +0100

    Update modele.py

--- machine_learning/modele.py
+++ machine_learning/modele.py
@@ -1,5 +1,5 @@
-import pandas as pd
 import os
+import pandas as pd
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder
