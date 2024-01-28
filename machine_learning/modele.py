3bc3270e5caf77fa3c1f7cba9967094b196fb8a6
Abicakci <121668685+Abicakci@users.noreply.github.com>
2024-01-28 15:16:53 +0100

    Update modele.py

--- machine_learning/modele.py
+++ machine_learning/modele.py
@@ -8,82 +8,80 @@ import os
 import time
 from prometheus_client import start_http_server, Summary, Gauge
 
-# Définir des métriques Prometheus
+# Initialisation des métriques Prometheus
 REQUEST_TIME = Summary('request_processing_seconds', 'Time spent processing request')
 MODEL_ACCURACY = Gauge('model_accuracy', 'Accuracy of the ML model')
 
-# Démarrer le serveur de métriques pour Prometheus sur le port 8000
+# Démarrage du serveur de métriques pour Prometheus
 start_http_server(8000)
-print("Server for Prometheus metrics started on port 8000.")
+print("Serveur de métriques démarré sur le port 8000.")
 
-# Récupération de la variable d'environnement indiquant l'environnement actuel
+# Récupération de la variable d'environnement qui indique l'environnement actuel
 environment = os.environ.get("ML_ENVIRONMENT", "test")
-print(f"Sending data to the environment: {environment}")
+print(f"Envoi des données vers l'environnement : {environment}")
 
-# Attente avant de commencer, pour s'assurer que la base de données est prête
-print("Waiting for 20 seconds before starting...")
+# Pause pour s'assurer que la base de données est prête
+print("Attente de 20 secondes avant de commencer...")
 time.sleep(20)
 
-# Tentatives de connexion à la base de données
+# Tentative de connexion à la base de données
 max_attempts = 5
 attempt_count = 0
 
 while attempt_count < max_attempts:
     try:
-        print(f"Attempting to connect to the database, try {attempt_count + 1}")
+        print(f"Tentative de connexion à la base de données, essai {attempt_count + 1}")
         db = mysql.connector.connect(
             host="database-service",
             user="root",
             password="mysecretpassword",
             database="mlops_weather"
         )
-        print("Connected to MySQL")
+        print("Connecté à MySQL")
         cursor = db.cursor()
         break
     except Error as e:
-        print(f"Error connecting to MySQL: {e}")
+        print(f"Erreur lors de la connexion à MySQL: {e}")
         attempt_count += 1
         time.sleep(5)
 
 if attempt_count == max_attempts:
-    print("Failed to connect to MySQL after several attempts")
+    print("Échec de la connexion à MySQL après plusieurs tentatives")
     exit(1)
 
-# Chemin vers le fichier de données
-data_file_path = 'data/data_features_with_location.csv'
-print(f"Loading data from {data_file_path}")
-
 # Chargement des données
-df = pd.read_csv(data_file_path)
+chemin_fichier_donnees = 'data/data_features_with_location.csv'
+print(f"Chargement des données depuis {chemin_fichier_donnees}")
+df = pd.read_csv(chemin_fichier_donnees)
 df['date'] = pd.to_datetime(df[['year', 'month', 'day']])
-print("Data loaded successfully.")
+print("Données chargées avec succès.")
 
 # Encodage des variables catégorielles
 le = LabelEncoder()
 df['location_encoded'] = le.fit_transform(df['location'])
-print("Categorical variables encoded.")
+print("Variables catégorielles encodées.")
 
 # Préparation des données pour l'entraînement
 X = df.drop(['raintomorrow', 'year', 'month', 'day', 'location', 'date'], axis=1)
 y = df['raintomorrow']
-
-# Séparation des données en ensembles d'entraînement et de test
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
-print("Data split into training and test sets.")
+print("Données séparées en ensembles d'entraînement et de test.")
 
-# Entraînement du modèle et suivi du temps de traitement
+# Entraînement du modèle
 with REQUEST_TIME.time():
     model = RandomForestClassifier()
-    print("Starting model training...")
+    print("Début de l'entraînement du modèle...")
     model.fit(X_train, y_train)
-    print("Model trained.")
-
-    # Mise à jour de la métrique de précision
+    print("Modèle entraîné.")
     accuracy = model.score(X_test, y_test)
     MODEL_ACCURACY.set(accuracy)
-    print(f"Model accuracy: {accuracy}")
+    print(f"Précision du modèle : {accuracy}")
 
-# Prédiction et insertion dans la base de données
+# Prédiction
+predictions = model.predict(X_test)
+print("Prédictions réalisées.")
+
+# Insertion des prédictions dans la base de données
 try:
     for i in range(len(predictions)):
         date = df.iloc[i]['date']
@@ -98,16 +96,14 @@ try:
             insert_query = "INSERT INTO weather_predictions (date, location, prediction, accuracy) VALUES (%s, %s, %s, %s)"
             cursor.execute(insert_query, (date, location, prediction, accuracy))
             db.commit()
-            print(f"Prediction inserted for {date} and {location}.")
+            print(f"Insertion de la prédiction pour {date} et {location}.")
         else:
-            print(f"Entry for {date} and {location} already exists.")
+            print(f"Entrée pour {date} et {location} existe déjà.")
 except Error as e:
-    print(f"SQL Error: {e}")
+    print(f"Erreur SQL : {e}")
 finally:
-    # Fermeture de la connexion à la base de données
     if db.is_connected():
         cursor.close()
         db.close()
-        print("Database connection closed.")
-
-print("Predictions successfully made and recorded in the database.")
+        print("Connexion à la base de données fermée.")
+print("Prédictions effectuées avec succès et enregistrées dans la base de données.")
